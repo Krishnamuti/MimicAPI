53aa80e41c592bdd1185645f1f486fcab636e6d3
Krishnamuti <krishnamuti_2009@hotmail.com>
2020-08-30 13:45:27 -0300

    PalavrasController

---
+++ MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimicAPI.Database;
+using MimicAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicAPI.Controllers
+{
+    [Route("api/palavras")]
+    public class PalavrasController : ControllerBase
+    {
+        
+        public readonly MimicContext _banco;
+
+        public PalavrasController(MimicContext banco)
+        {
+            _banco = banco;
+        }
+
+        //api/palavras
+        [Route("")]
+        [HttpGet]
+        public ActionResult ObterTodas()
+        {
+            return Ok(_banco.Palavras);
+        }
+
+        //api/palavras/1
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult Obter(int id)
+        {
+            return Ok(_banco.Palavras.Find(id));
+        }
+
+        //api/palavras
+        [Route("")]
+        [HttpPost]
+        public ActionResult Cadastrar(Palavra palavra)
+        {
+            _banco.Palavras.Add(palavra);
+            return Ok();
+        }
+
+        //api/palavras/1
+        [Route("{id}")]
+        [HttpPut]
+        public ActionResult Atualizar(int id,Palavra palavra)
+        {
+            palavra.Id = id;
+            _banco.Palavras.Update(palavra);
+            return Ok();
+        }
+
+        //api/palavras/1
+        [Route("{id}")]
+        [HttpDelete]
+        public ActionResult Deletar(int id)
+        {
+            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+            return Ok();
+        }
+
+    }
+}
